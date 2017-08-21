221abeeab8c4e88886300654c79f96503624f782
andlin <andre@lindhjem.net>
2017-08-21 13:19:52 +0200

    Load emacs config from dotfiles dir.

--- emacs.el
+++ emacs.el
@@ -117,7 +117,7 @@
 
 ;; Add a directory to our load path so that when you `load` things
 ;; below, Emacs knows where to look for the corresponding file.
-(add-to-list 'load-path "~/.emacs-config/customizations")
+(add-to-list 'load-path "~/dotfiles/emacs-config/customizations")
 
 ;; Sets up exec-path-from-shell so that Emacs will use the correct
 ;; environment variables
