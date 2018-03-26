5175bb4f3d518380c51b77ab1763cb840ac708ad
andlin <andre@lindhjem.net>
2018-03-26 13:51:57 +0200

    Restructure org-mode to use a GTD system.
    See https://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html

--- emacs-config/customizations/orgmode-settings.el
+++ emacs-config/customizations/orgmode-settings.el
@@ -6,5 +6,27 @@
 
 (setq org-refile-targets '(
    (nil :maxlevel . 2)              ; refile to headings in the current buffer
-   (org-agenda-files :maxlevel . 2) ; refile to any of these files
+   ("~/Documents/gtd/projects.org" :maxlevel . 2)
+   ("~/Documents/gtd/someday.org" :maxlevel . 1)
+   ("~/Documents/gtd/tickler.org" :maxlevel . 2)
     ))
+
+(custom-set-variables
+ '(org-agenda-file-regexp "\\`[^.].*\\.org\\'\\|\\`[0-9]+\\'")
+ '(org-agenda-files
+   (quote
+    ("~/Documents/hjemmefront.org" "~/Documents/kantega/TODO.org" "~/Documents/journal" "~/Documents/gtd/inbox.org" "~/Documents/gtd/projects.org" "~/Documents/gtd/tickler.org"))
+   )
+)
+
+;; Capture templates
+(setq org-capture-templates '(
+                              ("t" "Todo [inbox]" entry (file+headline "~/Documents/gtd/inbox.org" "Tasks")
+                               "* TODO %i%?")
+                              ("T" "Tickler" entry (file+headline "~/Documents/gtd/tickler.org" "Tickler")
+                               "* %i%? \n %U")))
+
+;; Org Todo Keywords
+(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+
+;;(setq debug-on-error t)
